55e0aa59dde3b00314aff58b08dc123869c2e610
Jatin Foujdar <jatin321001@gmail.com>
2025-06-14 00:19:01 +0530

    Initial Commit

---
+++ Emoji-Art-IOS/Emoji_Art_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_Art_IOSApp.swift
+//  Emoji-Art-IOS
+//
+//  Created by jatin foujdar on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_Art_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
