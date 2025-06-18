2fdd91b9ad2ae6a3398dbd1563249983d4db9642
Jatin Foujdar <jatin321001@gmail.com>
2025-06-19 00:41:10 +0530

    drag emojis one by one commit

--- Emoji-Art-IOS/View/EmojiArtDocumentView.swift
+++ Emoji-Art-IOS/View/EmojiArtDocumentView.swift
@@ -39,17 +39,34 @@ struct EmojiArtDocumentView: View {
                         .position(emoji.position.in(geometry))
                 }
             }
-            .dropDestination(for: URL.self){urls, location in
-                return drop(urls, at: location, in: geometry)
+            .dropDestination(for: Sturldata.self){sturldata, location in
+                return drop(sturldata, at: location, in: geometry)
             }
         }
     }
-    private func drop(_ urls: [URL], at location: CGPoint, in geometry: GeometryProxy) -> Bool{
-        if let url = urls.first{
-            document.setBackground(url)
-            return true
+    private func drop(_ sturldatas: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+
+        for url in sturldatas {
+            switch url {
+            case .url(let url):
+                document.setBackground(url)
+               
+            case .string(let emoji):
+                document.addEmoji(emoji, at: emojiPosition(at: location, in: geometry), size: PaletteEmojiSize)
+                return true
+            default:
+                break
+            }
         }
         return false
+    }
+    
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy)-> Emoji.Position{
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(
+            x: Int(location.x - center.x),
+            y: Int(-(location.y - center.y))
+            )
     }
 }
 
